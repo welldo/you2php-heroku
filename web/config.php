aa621007600cd0e9b47ad763649f2afc8c7a5fd9
welldo <yang78@live.com>
2018-01-30 18:14:16 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDbCD1h4vgIoBwG8XIBCC9y0wcqC5B16OM');
-define('GJ_CODE', 'hk');
+define('GJ_CODE', 'tw');
 define('SITE_NAME', '小股灶 - yout*be 克隆');
 define('TITLENAME', '小股灶YT');
 define('EN2DEKEY', 'fAwqP8O3NW');
