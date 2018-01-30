91472e9ba9eae1dae8525de16acd0fd6f01b6cb0
welldo <yang78@live.com>
2018-01-30 17:47:00 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyDbCD1h4vgIoBwG8XIBCC9y0wcqC5B16OM');
+define('GJ_CODE', 'hk');
+define('SITE_NAME', '小股灶 - yout*be 克隆');
+define('TITLENAME', '小股灶YT');
+define('EN2DEKEY', 'fAwqP8O3NW');
+define('EMAIL', 'welldo@189.cn');
 ?>
